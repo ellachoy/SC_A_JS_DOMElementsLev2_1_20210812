ddd40ad9e7b7c96424a4a0db22344dc8a25c6029
Ella Choy <egal21de@gmail.com>
2021-08-12 19:54:29 +0200

    emoH

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+console.log("hiDom");
+
+function changeText(){
+    console.log("test");
+   console.log( document.getElementById("navHome"));
+   document.getElementById("navHome").style.backgroundColor = "pink";
+   document.getElementById("navHome").style.color = "black";
+   document.getElementById("navHome").innerHTML = "emoH"
+    
+}
